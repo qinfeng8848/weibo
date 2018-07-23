6148f14be923d3a7f401e0e5436ff17d27adb9ce
qinfeng8848 <q884822@gmail.com>
2018-07-23 17:20:37 +0800

    ActiveRecord

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_033441) do
+ActiveRecord::Schema.define(version: 2018_07_23_091205) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "userName"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
