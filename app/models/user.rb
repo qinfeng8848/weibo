ccc22dc549e70de1ca3b4a77dfe7501b1e1883ce
qinfeng8848 <q884822@gmail.com>
2018-07-23 18:33:32 +0800

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  validates :userName, presence: true, length:{maximum: 20}
+  EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, length:{maximum: 20}, format: {with: EMAIL },uniqueness:{case_sensitive: false}
 end
