33cf9308f114fbfe71a1f115385f533b3d5cbec5
qinfeng8848 <q884822@gmail.com>
2018-07-24 10:40:49 +0800

    add password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   validates :userName, presence: true, length:{maximum: 20}
   EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :email, presence: true, length:{maximum: 20}, format: {with: EMAIL },uniqueness:{case_sensitive: false}
+
+  has_secure_password
+  validates :password,length: {minimum: 6}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_091205) do
+ActiveRecord::Schema.define(version: 2018_07_24_023447) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2018_07_23_091205) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "passwrod_digest"
+    t.string "password_digest"
   end
 
 end
